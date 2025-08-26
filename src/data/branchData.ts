7c7bbb074aba43e974131c1deed72eadf770320d
Aura Tech <auratech.ly@gmail.com>
2025-08-26 02:37:07 +0200

    Update branch names and locations

--- src/data/branchData.ts
+++ src/data/branchData.ts
@@ -4,27 +4,27 @@ import { isWithinOperatingHours } from '../utils/timeUtils';
 export const branches: Branch[] = [
   {
     id: 'airport',
-    name: 'فرع طريق المطار',
+    name: 'مستر شيش - طريق المطار',
     area: 'طريق المطار',
-    address: 'طريق المطار، بجانب محطة الوقود الليبية',
+    address: 'طريق المطار مقابل مدرسة المهاجرين',
     phone: '091-2345678',
     deliveryTime: '25-35 دقيقة',
     isOpen: isWithinOperatingHours()
   },
   {
     id: 'dollar',
-    name: 'فرع حي الدولار',
-    area: 'حي الدولار',
-    address: 'حي الدولار، شارع الجمهورية الرئيسي',
+    name: 'مستر كرسبي',
+    area: 'بلعون',
+    address: 'بلعون مقابل جامعة العرب الطبية',
     phone: '091-3456789',
     deliveryTime: '20-30 دقيقة',
     isOpen: isWithinOperatingHours()
   },
   {
     id: 'balaoun',
-    name: 'فرع بلعون',
+    name: 'مستر شيش - بلعون',
     area: 'بلعون',
-    address: 'منطقة بلعون، قرب السوق المركزي',
+    address: 'بلعون بجوار جامعة العرب الطبية',
     phone: '091-4567890',
     deliveryTime: '30-40 دقيقة',
     isOpen: isWithinOperatingHours()
