bb35fee1325419eb8345dfe10a91018dd2c480de
Aura Tech <auratech.ly@gmail.com>
2025-08-28 17:10:16 +0200

    Fix button overflow outside card borders

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -242,7 +242,7 @@ export const MenuItem: React.FC<MenuItemProps> = ({ item, onAddToCart }) => {
           </div>
 
           {/* Add Button */}
-          <div className="mt-auto flex items-center justify-center">
+          <div className="mt-auto flex items-center justify-center pb-2">
             <button
               onClick={handleDesktopAddToCart}
               disabled={!isOpen}
