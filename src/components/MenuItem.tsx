3b8578902e80f39fc58f519e2cc9a397fa0d772c
Aura Tech <auratech.ly@gmail.com>
2025-08-29 19:56:26 +0200

    Fix mobile press animation trigger

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -140,7 +140,7 @@ export const MenuItem: React.FC<MenuItemProps> = ({ item, onAddToCart, onRemoveF
             ? `ring-2 ${isMisterCrispy ? 'ring-[#55421A]' : 'ring-[#781220]'} ring-opacity-50 ${isMisterCrispy ? 'bg-gradient-to-r from-[#55421A]/5 to-transparent' : 'bg-gradient-to-r from-[#781220]/5 to-transparent'} shadow-xl scale-[1.02]`
             : ''
         } ${
-          isPressing ? 'scale-95 shadow-inner bg-gray-50' : ''
+          isPressing ? 'scale-[0.98] bg-gray-100' : ''
         } ${
           hasAppeared ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
         }`}
