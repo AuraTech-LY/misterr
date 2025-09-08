71a803610680ad8bf4b0b5ab3ece16838a2dd2b1
Aura Tech <auratech.ly@gmail.com>
2025-09-08 19:57:58 +0200

    Fix مستر برجريتو header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -131,7 +131,7 @@ export const Header: React.FC<HeaderProps> = ({
       <div className="sticky top-0 z-40 px-3 sm:px-4 py-3 sm:py-4 lg:px-16 xl:px-32 2xl:px-48">
       <div className="container mx-auto">
         <div className={`text-white rounded-2xl sm:rounded-3xl shadow-2xl backdrop-blur-lg border border-white border-opacity-10 px-4 sm:px-6 py-3 sm:py-4 ${
-          selectedRestaurant?.name?.includes('مستر كريسبي') ? 'bg-[#55421A]' : 'bg-[#781220]'
+          selectedRestaurant?.name?.includes('مستر كريسبي') ? 'bg-[#55421A]' : selectedRestaurant?.name?.includes('مستر برجريتو') ? 'bg-[#E59F49]' : 'bg-[#781220]'
         }`}>
         <div className="flex justify-between items-center">
           <button 
