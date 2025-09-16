88d666e049c54338c5a96c8415e03b1a71a38888
Aura Tech <auratech.ly@gmail.com>
2025-09-16 17:15:39 +0200

    Fix navigation bar covering back button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -110,7 +110,7 @@ export const Header: React.FC<HeaderProps> = ({
     <>
       {/* Back Button - Above Navigation */}
       {showBackButton && (
-        <div className="bg-gray-50 px-3 sm:px-4 py-2 lg:px-16 xl:px-32 2xl:px-48">
+        <div className="bg-gray-50 px-3 sm:px-4 py-2 lg:px-16 xl:px-32 2xl:px-48 pt-16 sm:pt-2">
           <div className="container mx-auto">
             <button
               onClick={(e) => {
