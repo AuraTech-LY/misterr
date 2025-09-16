103c1015a8f9fbc24cd428acdeb73aef6f945d03
Aura Tech <auratech.ly@gmail.com>
2025-09-16 17:19:07 +0200

    Fix unterminated JSX contents errors

--- src/pages/BranchMenuPage.tsx
+++ src/pages/BranchMenuPage.tsx
@@ -181,7 +181,6 @@ export const BranchMenuPage: React.FC<BranchMenuPageProps> = ({ branchId }) => {
         onBackClick={handleBackToRestaurants}
       />
 
-      <main className="container mx-auto px-4 py-4 sm:py-8 lg:px-16 xl:px-32 2xl:px-48 pt-24 sm:pt-8">
       <main className="container mx-auto px-4 py-4 sm:py-8 lg:px-16 xl:px-32 2xl:px-48 pt-32 sm:pt-16">
         {/* Add bottom padding for mobile navigation */}
         <div className="pb-20 sm:pb-0">
