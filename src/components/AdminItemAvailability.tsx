d0d4a28a2b8dacc3473788bbaa2c09f01bce8907
Aura Tech <auratech.ly@gmail.com>
2025-11-09 22:44:49 +0200

    Updated AdminItemAvailability.tsx

--- src/components/AdminItemAvailability.tsx
+++ src/components/AdminItemAvailability.tsx
@@ -234,7 +234,7 @@ export default function AdminItemAvailability() {
                         <button
                           onClick={() => toggleAvailability(item.id, item.is_available)}
                           disabled={updating[item.id]}
-                          className={`relative inline-flex h-8 w-14 shrink-0 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-[#781220] focus:ring-offset-2 ${
+                          className={`relative inline-flex h-6 w-11 shrink-0 rounded-full p-0.5 transition-colors focus:outline-none focus:ring-2 focus:ring-[#781220] focus:ring-offset-2 ${
                             updating[item.id] ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'
                           } ${
                             item.is_available ? 'bg-green-500' : 'bg-red-500'
@@ -244,8 +244,8 @@ export default function AdminItemAvailability() {
                             <RefreshCw className="w-4 h-4 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-spin text-white" />
                           ) : (
                             <span
-                              className={`inline-block h-6 w-6 transform rounded-full bg-white shadow-lg transition-transform ${
-                                item.is_available ? 'translate-x-[26px]' : 'translate-x-[2px]'
+                              className={`inline-block h-5 w-5 rounded-full bg-white shadow-lg transition-transform duration-200 ${
+                                item.is_available ? 'translate-x-5' : 'translate-x-0'
                               }`}
                             />
                           )}
